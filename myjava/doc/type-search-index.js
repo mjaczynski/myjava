c7c933b94ce78bd9b1e18bae96791dd6765a283e
jaczynski <jaczynski@gurobi.com>
2024-08-24 17:28:21 -0700

    initial commit

---
+++ myjava/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"myjava","l":"User"}];updateSearchResults();
---
+++ myjava/src/myjava/Hello.java
@@ -0,0 +1,19 @@
+package myjava;
+
+public class Hello {
+
+	public static void printx(double d) {
+		System.out.print(d);
+	}
+	
+	public static void main(String[] args) {
+		int a = 1;
+		System.out.print("Hello");
+		int b = 2;
+		Math.abs(3.2);
+		printx(1.2);
+		User u = new User("a","b");
+		System.out.print(u);
+	}
+
+}
